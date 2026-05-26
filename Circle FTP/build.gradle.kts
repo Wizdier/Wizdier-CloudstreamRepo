4b0bd42e1974f3691c92059cb2a3b4682cedb3eb
Wizdier <81230118+Wizdier@users.noreply.github.com>
2026-05-26 23:48:40 +0600

    Initial Commit

---
+++ Circle FTP/build.gradle.kts
@@ -0,0 +1,35 @@
+version = 22
+
+android {
+    namespace = "com.wizdier"
+    buildFeatures {
+        buildConfig = true
+    }
+    defaultConfig {
+        buildConfigField(
+            "String",
+            "TMDB_API",
+            "\"${System.getenv("TMDB_API") ?: ""}\""
+        )
+    }
+}
+
+cloudstream {
+    description = "Largest FTP site in Bangladesh"
+    authors = listOf("Wizdier")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+        "Cartoon",
+        "AsianDrama",
+        "Others",
+        "Documentary",
+    )
+    language = "bn"
+    iconUrl = "http://new.circleftp.net/static/media/logo.fce2c9029060a10687b8.png"
+    isCrossPlatform = true
+}
